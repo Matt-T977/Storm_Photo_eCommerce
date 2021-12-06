b6321ce4ffdb7f3c62b5382255a5aecf19a13487
Matt Taylor <89653410+Matt-T977@users.noreply.github.com>
2021-12-06 16:06:27 -0600

    Backend Express Initial Set Up
    Backend Express Initial Set Up

---
+++ backend/index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const app = express();
+
+app.get('/', function(req, res) {
+    return res.json({
+        message: "Hellow World",
+        success: true,
+    })
+});
+
+app.post('/', function(req, res) {
+    return res.json({
+        message: "Welcome",
+        success: true,
+    })
+});
+
+app.listen(8000, () =>
+    console.log('Listening on port 8000.'),);
