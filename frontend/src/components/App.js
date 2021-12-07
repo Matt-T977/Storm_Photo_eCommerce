237966ee215b1519f79b6cb68f13891b639d5784
Matt Taylor <89653410+Matt-T977@users.noreply.github.com>
2021-12-07 16:02:07 -0600

    Todo List for Front End

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -2,7 +2,16 @@ import React from 'react';
 import Navbar from './Navbar';
 import { Grid } from '@mui/material'
 import '../styles/App.css';
-
+// TODO: Add Router
+// TODO: Landing Page
+// TODO: Product Page
+// TODO: Product List
+// TODO: Cart
+// TODO: Profile Page
+// TODO: Payment
+// TODO: Auth
+// TODO: Account Creation
+// TODO: Log In
 
 function App() {
   return (
@@ -10,7 +19,7 @@ function App() {
       <Grid item xs={12}>
         <Navbar />
       </Grid>
-      <Grid item xs={8}>
+      <Grid item xs={12}>
         Hello World
       </Grid>
     </Grid>
