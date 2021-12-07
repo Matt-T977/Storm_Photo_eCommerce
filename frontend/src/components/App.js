4edd1864a15eda390913dc263b404942e1ebca38
Matt Taylor <89653410+Matt-T977@users.noreply.github.com>
2021-12-07 14:59:28 -0600

    Navbar set up

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Navbar from './Navbar';
 import { Grid } from '@mui/material'
 import '../styles/App.css';
 
@@ -6,6 +7,9 @@ import '../styles/App.css';
 function App() {
   return (
     <Grid container>
+      <Grid item xs={12}>
+        <Navbar />
+      </Grid>
       <Grid item xs={8}>
         Hello World
       </Grid>
