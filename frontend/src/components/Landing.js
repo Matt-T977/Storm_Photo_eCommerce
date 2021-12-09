d170785ccfc7312164173ccc74df0aa744bcbcf5
Matt Taylor <89653410+Matt-T977@users.noreply.github.com>
2021-12-09 16:51:27 -0600

    Landing Image added

--- frontend/src/components/Landing.js
+++ frontend/src/components/Landing.js
@@ -1,17 +1,19 @@
 import { Card, Grid, Paper, Typography } from '@mui/material'
 import React, { useState, useEffect } from 'react'
+import LandingImage from '../img/LandingImage.jpg'
 
 
 export default function Landing () {
     return (
         <Grid container justifyContent='center'>
-            <Grid xs={12} align='center' marginBottom={3}>
-                Photo Goes here
+            <Grid xs={12} align='center' marginBottom={3} >
+                <img src={LandingImage} alt='Storm Header'/>
             </Grid>
             <Grid md={10} xs={12} marginBottom={3}>
                 <Paper elevation={3}>
                     <Typography padding={3}>
                         About Section
+                        This the landing page of Shear Beauty
                     </Typography>
                 </Paper>
             </Grid>
