e05054b4345608ed2bbbc568a562a78a4514db87
Matt Taylor <89653410+Matt-T977@users.noreply.github.com>
2021-12-21 14:52:01 -0600

    Update Product Route with CRUD Layout

--- backend/src/routes/products.js
+++ backend/src/routes/products.js
@@ -2,7 +2,25 @@ import express from 'express';
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
-  res.send('Products List');
+// Product List CRUD
+router.get('/product/list', (req, res) => {
+  res.send('products list');
 });
+
+router.post('product/list', (req, res) => {
+  res.send('posted new product');
+});
+
+// Individual Product CRUD
+router.get('/product/{id}', (req, res) => {
+  res.send('Get Individual Product');
+});
+
+router.put('/product/{id}', (req, res) => {
+  res.send('Product Update');
+});
+
+router.delete('/product/{id}', (req, res) => {
+  res.send('delete a product');
+})
 export default router;
