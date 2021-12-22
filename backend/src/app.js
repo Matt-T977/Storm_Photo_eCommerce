a31b4949d2ac84fc783314ef4c59a7ea8bbd3c92
Matt Taylor <89653410+Matt-T977@users.noreply.github.com>
2021-12-22 15:29:52 -0600

    Initializing Firebase Backend

--- backend/src/app.js
+++ backend/src/app.js
@@ -3,7 +3,8 @@ import express from 'express';
 import path from 'path';
 import cookieParser from 'cookie-parser';
 import logger from 'morgan';
-import { initializeApp } from 'firebase-admin';
+import { initializeApp, applicationDefault, cert } from 'firebase-admin';
+import { getFirestore, Timestamp, FieldValue } from 'firebase-admin/firestore';
 
 import indexRouter from './routes/index';
 import usersRouter from './routes/users';
@@ -11,6 +12,10 @@ import productsRouter from './routes/products';
 import notificationsRouter from './routes/notifications';
 
 const app = express();
+
+// Firebase Initialize
+initializeApp();
+const db = getFirestore();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
