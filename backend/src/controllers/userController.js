042d9f456ed559be2b77211aa2021a4e536bf965
Matt Taylor <89653410+Matt-T977@users.noreply.github.com>
2021-12-27 16:33:38 -0600

    Prettier Formatting

--- backend/src/controllers/userController.js
+++ backend/src/controllers/userController.js
@@ -1,65 +1,67 @@
-import { firestore } from "firebase-admin";
-import db from "../db";
+import { firestore } from 'firebase-admin';
+import db from '../db';
 import User from '../models/user';
 
 // Post Request
 const addUser = async (req, res, next) => {
-    try {
-        const data = req.body;
-        const docRef = await db.collection('users').add(data)
-        res.status(200).send('User successfully created with profile: ' + docRef)
-    } catch (error) {
-        res.status(400).send(error.message);
-    } next
+  try {
+    const data = req.body;
+    const docRef = await db.collection('users').add(data);
+    res.status(200).send('User successfully created with profile: ' + docRef);
+  } catch (error) {
+    res.status(400).send(error.message);
+  }
+  next;
 };
 
 // Get All Users Request
 const getAllUsers = async (req, res, next) => {
-    try {
-        const users = await db.collection('users');
-        const data = await users.get();
-        const usersArray = []
-        if(data.empty) {
-            res.status(404).send('No users found.');
-        } else {
-            data.forEach(doc => {
-                const user = new User(
-                    doc.id,
-                    doc.data().firstName,
-                    doc.data().lastName,
-                    doc.data().streetAddress,
-                    doc.data().city,
-                    doc.data().state,
-                    doc.data().zipCode,
-                    doc.data().phoneNumber,
-                    doc.data().orders,
-                )
-                usersArray.push(user);
-            });
-            res.send(usersArray);
-        }
-    } catch (error) {
-        res.status(400).send(error.message);
-    } next
+  try {
+    const users = await db.collection('users');
+    const data = await users.get();
+    const usersArray = [];
+    if (data.empty) {
+      res.status(404).send('No users found.');
+    } else {
+      data.forEach((doc) => {
+        const user = new User(
+          doc.id,
+          doc.data().firstName,
+          doc.data().lastName,
+          doc.data().streetAddress,
+          doc.data().city,
+          doc.data().state,
+          doc.data().zipCode,
+          doc.data().phoneNumber,
+          doc.data().orders,
+        );
+        usersArray.push(user);
+      });
+      res.send(usersArray);
+    }
+  } catch (error) {
+    res.status(400).send(error.message);
+  }
+  next;
 };
 
 const getUser = async (req, res, next) => {
-    try {
-        const id = req.params.id;
-        const user = await db.collection('users').doc(id);
-        const data = await user.get();
-        if (!data.exists) {
-            res.status(404).send("This user does not exist.");
-        } else {
-            res.send(data.data());
-        }
-    } catch (error) {
-        res.status(400).send(error.message);
+  try {
+    const id = req.params.id;
+    const user = await db.collection('users').doc(id);
+    const data = await user.get();
+    if (!data.exists) {
+      res.status(404).send('This user does not exist.');
+    } else {
+      res.send(data.data());
     }
-}
+  } catch (error) {
+    res.status(400).send(error.message);
+  }
+};
 
 export default {
-    addUser,
-    getAllUsers,
-    getUser,
-}
+  addUser,
+  getAllUsers,
+  getUser,
+};
--- backend/src/db.js
+++ backend/src/db.js
@@ -1,14 +1,14 @@
 import { initializeApp } from 'firebase-admin/app';
-import { getFirestore} from 'firebase-admin/firestore';
+import { getFirestore } from 'firebase-admin/firestore';
 import config from '../config';
-import admin from 'firebase-admin'
-import serviceAccount from '../serviceAccountKey.json'
+import admin from 'firebase-admin';
+import serviceAccount from '../serviceAccountKey.json';
 
 // Firebase Initialize
 // const firebaseApp = initializeApp(config.firebaseConfig);
 const firebaseApp = admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount)
-})
+  credential: admin.credential.cert(serviceAccount),
+});
 const db = getFirestore();
 
-export default db
+export default db;
