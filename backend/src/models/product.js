1812143c86d3a07d7a45ccddcf7c70f6dd80ab62
Matt Taylor <89653410+Matt-T977@users.noreply.github.com>
2021-12-29 15:32:27 -0600

    Product Model

---
+++ backend/src/models/product.js
@@ -0,0 +1,12 @@
+class Product {
+  constructor(id, name, type, resolution, description, price) {
+    this.id = id;
+    this.name = name;
+    this.type = type;
+    this.resolution = resolution;
+    this.description = description;
+    this.price = price;
+  }
+}
+
+export default Product;
