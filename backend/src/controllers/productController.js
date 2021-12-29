a330e66d5745733afa81e9000b012e7fbdf983f0
Matt Taylor <89653410+Matt-T977@users.noreply.github.com>
2021-12-29 17:51:20 -0600

    Update Product Functionality

--- backend/src/controllers/productController.js
+++ backend/src/controllers/productController.js
@@ -47,7 +47,7 @@ const getAllProducts = async (req, res, next) => {
 const getProduct = async (req, res, next) => {
   try {
     const { id } = req.params;
-    const product = db.collection('products').doc(id);
+    const product = await db.collection('products').doc(id);
     const data = await product.get();
     if (!data.exists) {
       res.status(404).send('No product Found');
@@ -59,6 +59,19 @@ const getProduct = async (req, res, next) => {
   }
   next;
 };
+
+const updateProduct = async (req, res, next) => {
+    try {
+        const { id } = req.params;
+        const data = req.body;
+        const product = await db.collection('products').doc(id);
+        await product.update(data);
+        res.send('Product profile updated.')
+    } catch (error) {
+        res.status(400).send(error.message));
+    }
+    next;
+}
 
 export default {
   addProduct,
