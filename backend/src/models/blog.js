40a58ab9c265da8cffd237850ab204bb9d2e5a05
Matt Taylor <89653410+Matt-T977@users.noreply.github.com>
2022-01-03 15:16:25 -0600

    Backend: Blog Model created

---
+++ backend/src/models/blog.js
@@ -0,0 +1,12 @@
+class Blog {
+  constructor(id, userId, blogName, mediaURI, body, date) {
+    this.id = id;
+    this.userId = userId;
+    this.blogName = blogName;
+    this.mediaURI = mediaURI;
+    this.body = body;
+    this.date = date;
+  }
+}
+
+export default Blog;
