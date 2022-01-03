aa3d391c078eb393edc2c850d349dbcf58d7f37a
Matt Taylor <89653410+Matt-T977@users.noreply.github.com>
2022-01-03 15:35:21 -0600

    Backend: Post and Get All Blog Post Functionality

---
+++ backend/src/controllers/blogController.js
@@ -0,0 +1,45 @@
+import db from '../db';
+import Blog from '../models/blog';
+
+// Post Request
+const addBlog = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const data = req.body;
+    const user = await db.collection('users').doc(id);
+    await db.collection('users').doc(user.id).collection('blog').add(data);
+    res.status(200).send('Blog successfully created.');
+  } catch (error) {
+    res.status(400).send(error.message);
+  }
+  next;
+};
+
+// Get all Blog Post
+const getAllBlogPosts = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const blogs = await db.collection('users').doc(id).collection('blog');
+    const data = await blogs.get();
+    const blogArray = [];
+    if (data.empty) {
+      res.status(404).send('No Blog Posts.');
+    } else {
+      data.forEach((doc) => {
+        const blog = new Blog(
+          doc.id,
+          doc.data().userId,
+          doc.data().blogName,
+          doc.data().mediaURI,
+          doc.data().body,
+          doc.data().date,
+        );
+        blogArray.push(blog);
+      });
+      res.send(blogArray);
+    }
+  } catch (error) {
+    res.status(400).send(error.message);
+  }
+  next;
+};
