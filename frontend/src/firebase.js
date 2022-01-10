ea09e6e8916a43a23c406caef31fc8977745e9d4
Matt Taylor <89653410+Matt-T977@users.noreply.github.com>
2022-01-10 12:01:10 -0600

    Frontend Firebase Configuration

---
+++ frontend/src/firebase.js
@@ -0,0 +1,7 @@
+import { initializeApp } from "firebase/app";
+//import { getAnalytics } from "firebase/analytics";
+import firebaseConfig from "../config";
+
+const app = initializeApp(firebaseConfig);
+
+export default app;
