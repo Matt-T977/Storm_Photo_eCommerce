b84f03cbb5bc0caf49e46471dba6d2536a381113
Matt Taylor <89653410+Matt-T977@users.noreply.github.com>
2022-01-12 19:01:10 -0600

    Upload product included in product page.

--- frontend/src/components/PhotoList.js
+++ frontend/src/components/PhotoList.js
@@ -1,16 +1,30 @@
 import React, { useState, useEffect } from "react";
-import { Card, CardMedia, Grid, Typography, Paper } from "@mui/material";
+import {
+  Card,
+  CardMedia,
+  Grid,
+  Typography,
+  Paper,
+  IconButton,
+  CardContent,
+} from "@mui/material";
 import { ref } from "firebase/storage";
+import UploadProduct from "./UploadProduct";
 
 export default function PhotoList(props) {
   return (
     <Grid container justifyContent="center">
       <Grid item md={10} xs={12}>
-        <Paper>
-          <Typography variant="h4" component="h2">
-            This is the photo list.
-          </Typography>
-        </Paper>
+        <IconButton>
+          <UploadProduct />
+        </IconButton>
+        <Card>
+          <CardContent>
+            <Typography variant="h4" component="h2">
+              This is the photo list.
+            </Typography>
+          </CardContent>
+        </Card>
       </Grid>
     </Grid>
   );
