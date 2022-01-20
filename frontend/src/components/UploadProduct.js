989c1afe9f1b0f3b452cb9e97276d5e6a0a41c79
Matt Taylor <89653410+Matt-T977@users.noreply.github.com>
2022-01-20 08:31:06 -0600

    Frontend: Upload Product Modal Conversion start

--- frontend/src/components/UploadProduct.js
+++ frontend/src/components/UploadProduct.js
@@ -1,8 +1,9 @@
-import { Grid, TextField } from "@mui/material";
+import { Grid, Modal, TextField } from "@mui/material";
 import { Box } from "@mui/system";
 import React, { useState, useEffect } from "react";
 
 export default function UploadProduct() {
+  const [open, setOpen] = useState(false);
   const [productDetails, setProductDetails] = useState({
     imageURI: "",
     name: "",
@@ -30,69 +31,71 @@ export default function UploadProduct() {
     console.log({ productDetails, imageFile });
   };
   return (
-    <Grid container>
-      <Box component="form">
-        <Grid item>
-          <TextField
-            required
-            id="outlined-required"
-            name="image"
-            label="Image File"
-            type="file"
-            value={imageFile}
-            onChange={handleUpload}
-          />
-        </Grid>
-        <Grid item>
-          <TextField
-            required
-            id="outlined-required"
-            name="type"
-            label="Type"
-            type="text"
-            color="primary"
-            value={productDetails.type}
-            onChange={handleChange}
-            placeholder="Photo"
-          />
-        </Grid>
-        <Grid item>
-          <TextField
-            id="outlined"
-            name="resolution"
-            label="Resolution"
-            type="text"
-            color="primary"
-            value={productDetails.resolution}
-            onChange={handleChange}
-            placeholder="1920x1080"
-          />
-        </Grid>
-        <Grid item>
-          <TextField
-            id="outlined"
-            name="description"
-            label="Description"
-            type="text"
-            color="primary"
-            value={productDetails.description}
-            onChange={handleChange}
-            placeholder="This is a description of the product."
-          />
-        </Grid>
-        <Grid item>
-          <TextField
-            id="outlined"
-            name="price"
-            label="Price"
-            type="text"
-            color="primary"
-            value={productDetails.price}
-            onChange={handleChange}
-            placeholder="19.99"
-          />
-        </Grid>
-      </Box>
-    </Grid>
+    <Modal>
+      <Grid container>
+        <Box component="form">
+          <Grid item>
+            <TextField
+              required
+              id="outlined-required"
+              name="image"
+              label="Image File"
+              type="file"
+              value={imageFile}
+              onChange={handleUpload}
+            />
+          </Grid>
+          <Grid item>
+            <TextField
+              required
+              id="outlined-required"
+              name="type"
+              label="Type"
+              type="text"
+              color="primary"
+              value={productDetails.type}
+              onChange={handleChange}
+              placeholder="Photo"
+            />
+          </Grid>
+          <Grid item>
+            <TextField
+              id="outlined"
+              name="resolution"
+              label="Resolution"
+              type="text"
+              color="primary"
+              value={productDetails.resolution}
+              onChange={handleChange}
+              placeholder="1920x1080"
+            />
+          </Grid>
+          <Grid item>
+            <TextField
+              id="outlined"
+              name="description"
+              label="Description"
+              type="text"
+              color="primary"
+              value={productDetails.description}
+              onChange={handleChange}
+              placeholder="This is a description of the product."
+            />
+          </Grid>
+          <Grid item>
+            <TextField
+              id="outlined"
+              name="price"
+              label="Price"
+              type="text"
+              color="primary"
+              value={productDetails.price}
+              onChange={handleChange}
+              placeholder="19.99"
+            />
+          </Grid>
+        </Box>
+      </Grid>
+    </Modal>
   );
 }
